49560c38f11bac7b1c7b88a209aab97384e77148
Andrew Valentine <andrew.valentine@anu.edu.au>
2020-06-06 17:01:10 +1000

    Added some additional explanation

--- userSettings.py
+++ userSettings.py
@@ -1,5 +1,12 @@
+# Edit these settings as necessary. You may wish to run the command
+# git update-index --skip-worktree userSettings.py
+# to prevent your changes being tracked by git.
+#
 # Location of raw data file
-datafile = '../Davies_etal_NGeo_2019_Datasets/hoggard/spot_shiptrack.dat'
+datafile = './Davies_etal_NGeo_2019_Datasets/hoggard/spot_shiptrack.dat'
+# If you have just obtained this package from github you may need to run the command
+# git submodule update --init --recursive
+# to also pull down a copy of the data.
 
 # Base location for outputs
 # The code will create subdirectories under this for each data subset and
@@ -7,7 +14,7 @@ datafile = '../Davies_etal_NGeo_2019_Datasets/hoggard/spot_shiptrack.dat'
 outputdir = '.'
 
 # Run analysis for the subset of 1160 high-accuracy spot points?
-DO_HIGH_ACCURACY_SPOT = False
+DO_HIGH_ACCURACY_SPOT = True
 
 # Run analysis for the subset of all 2030 spot points?
 DO_ALL_SPOT = True
